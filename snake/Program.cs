34332bb8c3abedefe3d33e0000023223f046de10
Stashov <Stashov87@mail.ru>
2016-11-25 22:12:54 +0400

    начало

---
+++ snake/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace snake
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x1 = 1;
+            int y1 = 3;
+            char sym1 = '*';
+            Console.SetCursorPosition(x1, y1);
+            Console.Write(sym1);
+
+            Console.ReadLine();
+        }
+    }
+}
