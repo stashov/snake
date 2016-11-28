34cd6e300d7a1ab72f5a5e1ea12bbccfe160abf6
Stashov <Stashov87@mail.ru>
2016-11-28 21:58:36 +0400

    добвил конструктор в класс

--- snake/Point.cs
+++ snake/Point.cs
@@ -11,5 +11,18 @@ namespace snake
         public int x;
         public int y;
         public char sym;
+
+        public Point(int _x, int _y, char _sym)
+        {
+            x = _x;
+            y = _y;
+            sym = _sym;
+        }
+
+        public void Draw()
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(sym);
+        }
     }
 }
